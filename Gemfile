b836885b90bb9982c2a9a32f31b15bc6562c35e7
srizzo <rizzolabs@gmail.com>
2024-06-25 14:28:21 +0200

    Lambda auth to github

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ source "https://rubygems.org"
 gem "octokit", "~> 9.1"
 
 gem "aws-sdk-s3", "~> 1.153"
+
+gem "jwt", "~> 2.8"
--- lambda_function.rb
+++ lambda_function.rb
@@ -1,18 +1,58 @@
-require 'aws-sdk-s3'
 require 'json'
+require 'base64'
+require 'octokit'
+require 'openssl'
+require 'jwt'
 
 def lambda_handler(event:, context:)
-  puts "Service gem version: #{Aws::S3::GEM_VERSION}"
-  puts "Core version: #{Aws::CORE_GEM_VERSION}"
-  
-  puts "Received event:"
-  puts JSON.pretty_generate(event)
-  
-  # You can add more specific logging here if needed
-  
+  # Parse the incoming GitHub webhook payload
+  body = JSON.parse(event['body'])
+
+  # Log the event type
+  puts "Received GitHub event: #{event['headers']['X-GitHub-Event']}"
+
+  # Log the full payload
+  puts "Payload: #{body.to_json}"
+
+  # Decode the base64-encoded private key
+  private_key = Base64.decode64(ENV['GITHUB_PRIVATE_KEY'])
+
+  # Set up Octokit client with JWT authentication
+  app_id = ENV['GITHUB_APP_ID'] # Make sure to set this in your Lambda environment variables
+  client = create_jwt_client(app_id, private_key)
+
+  # Use the client to get information about the authenticated app
+  app_info = client.app
+
+  puts "Authenticated as GitHub App: #{app_info.name}"
+
+  # You can add more specific handling for different event types here
+
   # Return a response
   {
     statusCode: 200,
-    body: JSON.generate({message: "Event logged successfully"})
+    body: JSON.generate({
+                          message: "GitHub event processed successfully",
+                          app_name: app_info.name
+                        })
   }
+end
+
+def create_jwt_client(app_id, private_key)
+  private_key = OpenSSL::PKey::RSA.new(private_key)
+
+  # Generate the JWT
+  payload = {
+    # issued at time, 60 seconds in the past to allow for clock drift
+    iat: Time.now.to_i - 60,
+    # JWT expiration time (10 minute maximum)
+    exp: Time.now.to_i + (10 * 60),
+    # GitHub App's identifier
+    iss: app_id
+  }
+
+  jwt = JWT.encode(payload, private_key, "RS256")
+
+  # Create an Octokit client with the JWT
+  Octokit::Client.new(bearer_token: jwt)
 end
