1e08b14d55b5179ffd9a865a4f2c49ef4ef2357f
srizzo <rizzolabs@gmail.com>
2024-06-25 13:55:59 +0200

    Log events

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,6 +1,18 @@
 require 'aws-sdk-s3'
+require 'json'
 
 def lambda_handler(event:, context:)
   puts "Service gem version: #{Aws::S3::GEM_VERSION}"
   puts "Core version: #{Aws::CORE_GEM_VERSION}"
+  
+  puts "Received event:"
+  puts JSON.pretty_generate(event)
+  
+  # You can add more specific logging here if needed
+  
+  # Return a response
+  {
+    statusCode: 200,
+    body: JSON.generate({message: "Event logged successfully"})
+  }
 end
