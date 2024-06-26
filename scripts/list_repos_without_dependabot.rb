3cbee0da7ddfc5d6cbf336a28ecda96f555183ea
srizzo <rizzolabs@gmail.com>
2024-06-26 15:41:49 +0200

    list backend repos

--- scripts/list_repos_without_dependabot.rb
+++ scripts/list_repos_without_dependabot.rb
@@ -4,11 +4,19 @@ client = github_app_installation_client(ENV['TASKMATES_INSTALLATION_ID'])
 
 results = client.list_app_installation_repositories
 
-repos_without_dependabot = results[:repositories].reject do |repo|
-  client.contents(repo.full_name, path: '.github/dependabot.yml')
-rescue Octokit::NotFound
-  false
+repos_with_dependabot = []
+repos_without_dependabot = []
+
+results[:repositories].each do |repo|
+  begin
+    client.contents(repo.full_name, path: '.github/dependabot.yml')
+    repos_with_dependabot << repo
+  rescue Octokit::NotFound
+    repos_without_dependabot << repo
+  end
 end
 
+puts "Repos with Dependabot: #{repos_with_dependabot.map(&:full_name)}"
 puts "Repos without Dependabot: #{repos_without_dependabot.map(&:full_name)}"
+
 
